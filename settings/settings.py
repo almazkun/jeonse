8abaf4b680cb8b964dd99a5637b4a37642008ab8
Almaz Kunpeissov <almaz.kun@gmail.com>
2022-05-31 14:42:54 +0900

    whitenise setup

--- settings/settings.py
+++ settings/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -128,14 +129,16 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-STATIC_ROOT
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# https://whitenoise.evans.io/en/stable/
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 # https://docs.djangoproject.com/en/4.0/topics/logging/
 LOGGING = {
