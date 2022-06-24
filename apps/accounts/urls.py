f234d6f621002dd6409b9e0eb8c17984797e698e
Almaz Kunpeissov <almaz.kun@gmail.com>
2022-06-24 11:27:12 +0900

    Removing CustomUser

--- apps/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from apps.accounts.views import SigninView, SignoutView, SignupView
-
-urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("signin/", SigninView.as_view(), name="signin"),
-    path("signout/", SignoutView.as_view(), name="signout"),
-]
