70efd4abbea15a2d6a90658003025826f17e58e1
Almaz Kunpeissov <almaz.kun@gmail.com>
2022-06-24 17:10:14 +0900

    lint

--- jeonse/management/commands/create_demo_user.py
+++ jeonse/management/commands/create_demo_user.py
@@ -1,7 +1,10 @@
+from uuid import uuid4
+
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
-from uuid import uuid4
+
+
 class Command(BaseCommand):
     help = "Creates Demo user"
 
@@ -11,7 +14,9 @@ class Command(BaseCommand):
 
         if email and password:
             if not get_user_model().objects.filter(email=email).exists():
-                user = get_user_model().objects.create_user(email=email, username=uuid4().hex)
+                user = get_user_model().objects.create_user(
+                    email=email, username=uuid4().hex
+                )
                 user.set_password(password)
                 user.save()
             self.stdout.write(
